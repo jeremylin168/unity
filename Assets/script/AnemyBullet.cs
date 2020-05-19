e6b1f360f5a0caea05680c321cc8981e336bc0b2
jeremylin168 <jeremylin168@gmail.com>
2020-05-19 20:27:56 +0800

    enemy fire
    add enemy

---
+++ Assets/script/AnemyBullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnemyBullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int damage = 10;
+
+    public float speed=20f;
+    public float range = 10f;
+
+    private float lifetime;
+    public Audio_control Audio;
+    void Start()
+    {
+        lifetime = range / speed;       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0f)
+        {
+            Kill();
+        }   
+    }
+    
+    public void Kill()
+    {
+        Destroy(gameObject);
+    }
+    public float fire(){
+        Audio.PlayGunShot();
+        gameObject.SetActive(true);
+        return speed;
+    }
+}
--- Assets/script/AnenyControl.cs
+++ Assets/script/AnenyControl.cs
@@ -9,11 +9,15 @@ public class AnenyControl : MonoBehaviour
     public GameObject AnBody;
     public float speed;
     private Rigidbody2D AnRig;
-    private float x,sq,angle;
+    private float x,sq,angle,t;
     private Vector3 pos;
+    public AnemyBullet Anemy_bullet;
+
+    public float fire_rate;
     void Start()
     {
         AnRig=GetComponent<Rigidbody2D>();
+        t=fire_rate;
 
     }
 
@@ -23,7 +27,20 @@ public class AnenyControl : MonoBehaviour
         pos=playerpos.pos();
         angle=Mathf.Atan2(pos.y-transform.position.y,pos.x-transform.position.x);
         AnBody.GetComponent<Transform>().transform.rotation = Quaternion.Euler(0f,0f,angle* Mathf.Rad2Deg);
-
         AnRig.velocity= new Vector2(Mathf.Cos(angle)*speed,Mathf.Sin(angle)*speed);
+        if(t<0){
+            fire();
+            t=fire_rate;
+        }
+        t-=Time.deltaTime;
+
+    }
+    private void fire(){
+        
+        AnemyBullet b=Instantiate(Anemy_bullet,transform.localPosition+new Vector3(Mathf.Cos(angle),Mathf.Sin(angle),0),Quaternion.identity);
+        float bullet_speed = b.fire();
+        b.GetComponent<Rigidbody2D>().velocity=new Vector2(Mathf.Cos(angle)*bullet_speed,Mathf.Sin(angle)*bullet_speed);
+        
+
     }
 }
