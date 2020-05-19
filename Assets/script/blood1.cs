657cf651d7dc0df247c65d0e1074f2fc4b36a377
futaorfuku <air22682525@gmail.com>
2020-05-19 20:57:53 +0800

    3

---
+++ Assets/script/blood1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blood1 : MonoBehaviour
+{
+    public GameObject CURE;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(CURE);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/script/energy1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class energy1 : MonoBehaviour
+{
+    public float energy;
+    public PlayerControll s;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        //Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag == "Player")
+        {
+            s.add_energy(energy);
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
