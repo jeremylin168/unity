b7fee82435f3ed6a86ff7e29efa5925d7b621a17
jeremylin168 <jeremylin168@gmail.com>
2020-05-19 18:16:00 +0800

    AddEnergy
    Energy system added!

--- Assets/script/PlayerControll.cs
+++ Assets/script/PlayerControll.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerControll : MonoBehaviour, IHurtable
 {
     public float blood=100f;
+    public float energy=100f;
 
     public void Hurt(int damage)
     {
@@ -14,7 +15,6 @@ public class PlayerControll : MonoBehaviour, IHurtable
     // Start is called before the first frame update
     void Start()
     {
-        blood=100f;
     }
 
     // Update is called once per frame
@@ -26,6 +26,21 @@ public class PlayerControll : MonoBehaviour, IHurtable
         else if(blood<0){
             blood=0;
         }
+        if(energy>100){
+            energy=100;
+        }
+        else if(energy<0){
+            energy=0;
+        }      
         
     }
+     public void consume_energy(float a){
+         energy-=a;
+     }
+     public void add_energy(float a){
+         energy+=a;
+     }
+     public float get_energy(){
+         return energy;
+     }
 }
--- Assets/script/PlayerMove.cs
+++ Assets/script/PlayerMove.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public Item item;
+    public PlayerControll PlayerEnergy;
+
+    public float energy_consume=5,energy_regen=5;
+    private float t;
 
     private Rigidbody2D pl;
-    public float speed=1;
+    public float speed;
+    private float ospeed;
     private float x,y,sq;
     // Start is called before the first frame update
     void Start()
     {
-        speed=5;
+        ospeed=speed;
         pl=GetComponent<Rigidbody2D>();
         sq=Mathf.Sqrt(2);
     }
@@ -20,8 +25,8 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        x=Input.GetAxisRaw("Horizontal")*speed;
-        y=Input.GetAxisRaw("Vertical")*speed;
+        x=Input.GetAxisRaw("Horizontal")*ospeed;
+        y=Input.GetAxisRaw("Vertical")*ospeed;
         if(x!=0&&y!=0){
             x/=sq;
             y/=sq;
@@ -35,5 +40,28 @@ public class PlayerMove : MonoBehaviour
                 iu.Use(UtilInput.GetCursorWorldPos());
             }
         }
+        if(Input.GetKey(KeyCode.LeftShift)){
+            if(PlayerEnergy.get_energy()>0){
+            ospeed=2*speed;
+            PlayerEnergy.consume_energy(energy_consume*Time.deltaTime);
+            }
+            else{
+                t=1;
+                ospeed=speed;
+            }
+
+            
+        }
+        else{
+            ospeed=speed;
+            if(t<0){
+                PlayerEnergy.add_energy(energy_regen*Time.deltaTime);
+            }
+            else{
+                t-=Time.deltaTime;
+            }
+
+        }
+        
     }
 }
