1bf33531d2b3cd0184a84394e3c9cc9929a4fe8e
futaorfuku <air22682525@gmail.com>
2020-06-02 20:29:59 +0800

    2.4

--- Assets/script/ItemGun.cs
+++ Assets/script/ItemGun.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ItemGun : ItemUsable
 {
+    public float reloadTime = 1.5f;
+    public float reloadCD = 0f;
     public Bullet bullet;
 
     // Start is called before the first frame update
@@ -15,12 +17,21 @@ public class ItemGun : ItemUsable
     // Update is called once per frame
     void Update()
     {
-        
+        if (useCD < useTime) useCD += Time.deltaTime;
     }
 
     public override void Use(Vector2 worldPos)
     {
-        Bullet firedBullet = Instantiate(bullet, owner.transform.position, Quaternion.Euler(0f, 0f, 
+        if (useCD < useTime) return;
+
+        useCD -= useTime;
+        FireBullet(worldPos);
+    }
+
+    private void FireBullet(Vector2 worldPos)
+    {
+        Bullet firedBullet = Instantiate(
+            bullet, owner.transform.position, Quaternion.Euler(0f, 0f,
             UtilMath.Atan2(worldPos - (Vector2)owner.transform.position) * Mathf.Rad2Deg));
         firedBullet.Fire();
     }
